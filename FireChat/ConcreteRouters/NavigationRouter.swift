cfb4dc3354a9c2b61063d3d423387c575276635e
Petre Vane <PetreVane@users.noreply.github.com>
2020-03-05 09:42:26 +0100

    NavigationRouter initialised with a NavigationController

--- FireChat/ConcreteRouters/NavigationRouter.swift
+++ FireChat/ConcreteRouters/NavigationRouter.swift
@@ -8,39 +8,47 @@
 
 import UIKit
 
+
+
 class NavigationRouter: NSObject {
     
+    // base ViewController used by presetModally()
     unowned var baseViewController: UIViewController?
-    var navigationController = UINavigationController()
-    private var onDismissForViewController: Dictionary<UIViewController, (()-> Void)> = [:]
+    var navigationController: UINavigationController
+    private var onDismissForViewController: [UIViewController: (() -> Void)] = [:]
     
-    override init() {
+    init(navigationController: UINavigationController ) {
+        self.navigationController = navigationController
         super.init()
         navigationController.delegate = self
     }
 }
 
+
 extension NavigationRouter: Router {
-    
+
     func present(_ viewController: UIViewController, animated: Bool, onDismiss: (() -> Void)?) {
+
         onDismissForViewController[viewController] = onDismiss
-        
-        if baseViewController == nil {
-            navigationController.pushViewController(viewController, animated: animated)
-        } else {
+
+        if baseViewController != nil {
             //calls presentModally, if there is a new instance of NavigationRouter, which has the 'baseViewController' set.
+            print("Called NavigationRouter presentModally method for vc: \(viewController)")
             presentModally(viewController, animated: animated, onDismiss: onDismiss)
+        } else {
+           print("Called NavigationRouter present method for vc: \(viewController)")
+           navigationController.pushViewController(viewController, animated: animated)
         }
     }
-    
+
     func dismiss(animated: Bool) {
         guard let viewController = navigationController.viewControllers.first else { return }
         performOnDismissAction(for: viewController)
         baseViewController?.dismiss(animated: animated, completion: nil)
     }
-    
+
      // MARK: - Private methods
-    
+
     /// Presents a viewController modally
     /// - Parameters:
     ///   - viewController: ViewController to be presented
@@ -53,20 +61,20 @@ extension NavigationRouter: Router {
         addCancelButton(on: viewController)
         baseViewController?.present(navigationController, animated: animated, completion: nil)
     }
-    
+
     /// Adds a navigation bar button item
     /// - Parameter viewController: viewController that contains the button
     private func addCancelButton(on viewController: UIViewController) {
         let cancelButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(cancelButtonPressed))
         viewController.navigationItem.rightBarButtonItem = cancelButton
     }
-    
+
     @objc private func cancelButtonPressed() {
         guard let viewController = navigationController.viewControllers.first else { return }
         performOnDismissAction(for: viewController)
         baseViewController?.dismiss(animated: true, completion: nil)
     }
-    
+
     /// Executes a closure when ViewController is dismissed
     /// - Parameter viewController: ViewController which is being dismissed
     /// - makes sure there is an associated closure for the passed in argument
@@ -77,12 +85,11 @@ extension NavigationRouter: Router {
         onDismissAction()
         onDismissForViewController.removeValue(forKey: viewController)
     }
-    
+
 }
 
-
 extension NavigationRouter: UINavigationControllerDelegate {
-    
+
     func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
         guard let dismissedViewController = navigationController.transitionCoordinator?.viewController(forKey: .from) else { return }
         guard !navigationController.viewControllers.contains(dismissedViewController) else { return }
