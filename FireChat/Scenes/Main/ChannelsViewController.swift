58d6639a2d80e58e69be63489f68d0c3309643a3
Petre Vane <PetreVane@users.noreply.github.com>
2020-03-25 21:26:17 +0100

    Added label & constraints

--- FireChat/Scenes/Main/ChannelsViewController.swift
+++ FireChat/Scenes/Main/ChannelsViewController.swift
@@ -8,14 +8,44 @@
 
 import UIKit
 
-class ChannelsViewController: UIViewController {
+protocol ChannelsVCDelegate: AnyObject {
+    // didPressChatCell -> init ChatVC
+}
 
+class ChannelsViewController: UIViewController {
+    
+    weak var delegate: ChannelsVCDelegate?
+    private let label = FireLabel(textAlignment: .center, fontSize: 25)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .systemGreen
+        view.backgroundColor = .systemBackground
+        configureLabel()
     }
+    
+    private func configureLabel() {
+        view.addSubview(label)
+        label.text = "Welcome to Channels ViewController"
+        let padding: CGFloat = 50
+        label.backgroundColor = .systemBackground
+        
+        NSLayoutConstraint.activate([
+        
+            label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: padding),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            label.heightAnchor.constraint(equalToConstant: padding)
+        ])
+    }
+    
+}
 
-
+extension ChannelsViewController {
+    class func instantiate(delegate: ChannelsVCDelegate) -> ChannelsViewController {
+        let viewController = ChannelsViewController()
+        viewController.delegate = delegate
+        return viewController
+    }
 }
 
--- FireChat/Scenes/Main/ProfileViewController.swift
+++ FireChat/Scenes/Main/ProfileViewController.swift
@@ -8,15 +8,43 @@
 
 import UIKit
 
+protocol ProfileVCDelegate: AnyObject {
+    
+}
+
 class ProfileViewController: UIViewController {
 
+    weak var delegate: ProfileVCDelegate?
+    private let label = FireLabel(textAlignment: .center, fontSize: 25)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemTeal
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        configureLabel()
     }
     
 
+    private func configureLabel() {
+        view.addSubview(label)
+        label.text = "Welcome to Profile ViewController"
+        let padding: CGFloat = 50
+        label.backgroundColor = .systemBackground
+        
+        NSLayoutConstraint.activate([
+        
+            label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: padding),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            label.heightAnchor.constraint(equalToConstant: padding)
+        ])
+    }
 
+}
+
+extension ProfileViewController {
+    class func instantiate(delegate: ProfileVCDelegate) -> ProfileViewController {
+        let viewController = ProfileViewController()
+        viewController.delegate = delegate
+        return viewController
+    }
 }
