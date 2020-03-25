4457c1a69b452030d441e07bdb510c8da3cdb382
Petre Vane <PetreVane@users.noreply.github.com>
2020-03-25 21:25:38 +0100

    Removed reference to custom tabBar; MainApp Coordinator starts its corresponding view controllers

--- FireChat/ConcreteCoordinators/MainAppCoordinator.swift
+++ FireChat/ConcreteCoordinators/MainAppCoordinator.swift
@@ -19,10 +19,34 @@ class MainAppCoordinator: Coordinator {
     
     func start() {
         // starts this coordinator
-        _ = TabBar()
+//        _ = TabBar()
+        let tabBar = UITabBarController()
+        tabBar.viewControllers = [startChannelViewController(), startProfileViewController()]
     }
  
     func removeCoordinator(_ coordinator: Coordinator) {
         parentCoordinator?.removeCoordinator(self)
     }
+    
+    private func startChannelViewController() -> UINavigationController {
+        let channelsVC = ChannelsViewController.instantiate(delegate: self)
+        channelsVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 0)
+        
+        return UINavigationController(rootViewController: channelsVC)
+    }
+    
+    private func startProfileViewController() -> UINavigationController {
+        let profileVC = ProfileViewController.instantiate(delegate: self)
+        profileVC.tabBarItem = UITabBarItem(tabBarSystemItem: .contacts, tag: 1)
+        return UINavigationController(rootViewController: profileVC)
+    }
+}
+
+extension MainAppCoordinator: ChannelsVCDelegate {
+    
+}
+
+extension MainAppCoordinator: ProfileVCDelegate {
+    
+    
 }
