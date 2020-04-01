7bed3097a0ac4becfc79453f50db2089885e65d0
Petre Vane <PetreVane@users.noreply.github.com>
2020-04-01 15:42:00 +0200

    Added channel model; added references to channels within an enum file

---
+++ FireChat/Enums/FirebaseEnums.swift
@@ -0,0 +1,15 @@
+//
+//  FirebaseEnums.swift
+//  FireChat
+//
+//  Created by Petre Vane on 01/04/2020.
+//  Copyright © 2020 Petre Vane. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+
+enum Collection {
+    static let chatRooms = "ChatRooms"
+}
---
+++ FireChat/Models/Channel.swift
@@ -0,0 +1,15 @@
+//
+//  Channel.swift
+//  FireChat
+//
+//  Created by Petre Vane on 01/04/2020.
+//  Copyright © 2020 Petre Vane. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    
+    let title: String
+    let description: String
+}
