8636f024b383f3568c2c7f6335c8fc17020b1c1f
Petre Vane <PetreVane@users.noreply.github.com>
2020-04-12 18:43:56 +0200

    Changed how retrieved messages are ordered and stored

--- FireChat/Firebase/CloudFirestore.swift
+++ FireChat/Firebase/CloudFirestore.swift
@@ -16,6 +16,7 @@ final class CloudFirestore {
     private let cloudStorage = CloudStorage.shared
     private let auth = FirebaseAuth.shared
     var lastSnapshotForChatRoom: Dictionary<ChatRoom, QueryDocumentSnapshot> = [:]
+    var retrievedMessages = [Message]()
     
     //MARK: - Chat rooms
     
@@ -112,7 +113,7 @@ final class CloudFirestore {
     func fetchLatestMessages(for chatRoom: ChatRoom, completion: @escaping (Result<[Message], ErrorsManager>) -> Void) {
         
         var databaseQuery: Query?
-        var retrievedMessages = [Message]()
+        
         let chatRoomMessages = chatRooms.document(chatRoom.title).collection(Collection.chatMessages)
         
         if let lastSnapShot = lastSnapshotForChatRoom[chatRoom] {
@@ -129,6 +130,7 @@ final class CloudFirestore {
             guard let lastSnapShot = snapShot.documents.last else { return }
             self.lastSnapshotForChatRoom.updateValue(lastSnapShot, forKey: chatRoom)
             let documents = snapShot.documents
+            print("Documents so far: \(documents.count)")
             _ = documents.map { document in
 
                 let text = document["text"] as? String
@@ -140,15 +142,13 @@ final class CloudFirestore {
                 let userProvider = document["userProvider"] as? String
                 let messageDataURL = document["messageDataURL"] as? URL
 
-                let user = User(name: userName ?? "Missing userName", email: userEmail ?? "Missing email address",
-                                photoURL: userPhotoURL, provider: userProvider)
-                let cloudMessage = Message(text: text ?? "No text", user: user, messageID: messageID ?? "No message ID",
-                                           date: Date(timeIntervalSinceReferenceDate: timeInterval ?? 00))
+                let user = User(name: userName ?? "Missing userName", email: userEmail ?? "Missing email address", photoURL: userPhotoURL, provider: userProvider)
+                let cloudMessage = Message(text: text ?? "No text", user: user, messageID: messageID ?? "No message ID", date: Date(timeIntervalSinceReferenceDate: timeInterval ?? 00))
                 cloudMessage.messageDataURL = messageDataURL
-                guard !retrievedMessages.contains(cloudMessage) else { return }
-                retrievedMessages.append(cloudMessage)
-
-            };  completion(.success(retrievedMessages))
+                guard !self.retrievedMessages.contains(cloudMessage) else { return }
+                self.retrievedMessages.insert(cloudMessage, at: 0)
+                print("Retrieved messages: \(self.retrievedMessages.count)")
+            }; completion(.success(self.retrievedMessages))
         }
     }
 }
