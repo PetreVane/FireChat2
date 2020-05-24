b57b443a0d6598d3702c93baeb18d60af37ccd39
Petre Vane <PetreVane@users.noreply.github.com>
2020-05-24 15:39:28 +0200

    Updates keys

--- FireChat/Enums/FirebaseEnums.swift
+++ FireChat/Enums/FirebaseEnums.swift
@@ -22,6 +22,6 @@ enum CloudStorageDirectory {
 }
 
 enum ProjectCredentials {
-    static let serverKey = "key=AAAAMT2uHDY:APA91bGWNilCGGGZDmaVY0QGfyAinU-RCTOGaTM-aAKrCvfv1EHblUVAcaRgl370S0fXzdtoJselxoZjNpichOcPR3oMQzB-6YJzMFMdRs95IFEXIMKtrJbjoTZ0Vsw1sLARbJVe16zA"
-    static let senderID = "211488218166"
+    static let serverKey = "keychOcPR3oMQzB-6YJzMFMdRs95IFEXIMKtrJbjoTZ0Vsw1sLARbJVe16zA"
+    static let senderID = "45849343943094309430439430943094039403943"
 }
